6cd092c51dc8d1e63aa85505a30ea9ddf7eba2d4
Osigelialex <osigelialex@gmail.com>
2024-07-17 15:12:20 +0100

    handled errors

--- src/services/impl/paystack.service.impl.ts
+++ src/services/impl/paystack.service.impl.ts
@@ -7,22 +7,22 @@ const secretKey = process.env.PAYSTACK_SECRET_KEY;
 export class PaystackServiceImpl implements PaystackService {
 
   async createPaymentSession(amount: number, email: string, eventId: string, quantity: number): Promise<Object> {
-    console.log(email, amount, eventId, quantity);
-    const response = await axios.post("https://api.paystack.co/transaction/initialize", {
-      email,
-      amount: amount * 100,
-      metadata: { eventId, quantity }
-    }, {
-      headers: {
-        Authorization: `Bearer ${secretKey}`
-      }
-    });
+    try {
+      const response = await axios.post("https://api.paystack.co/transaction/initialize", {
+        email,
+        amount: amount * 100,
+        metadata: { eventId, quantity }
+      }, {
+        headers: {
+          Authorization: `Bearer ${secretKey}`
+        }
+      });
 
-    const authorizationUrl = response.data.data.authorization_url;
-    const reference = response.data.data.reference;
-    return { authorizationUrl, reference };
-  } catch (error: any) {
-    throw new CustomError(500, "Failed to create payment session");
+      const authorizationUrl = response.data.data.authorization_url;
+      const reference = response.data.data.reference;
+      return { authorizationUrl, reference };
+    } catch (error: any) {
+      throw new CustomError(500, "Failed to create payment session");
+    }
   }
-
 }
