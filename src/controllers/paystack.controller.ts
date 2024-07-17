4d9542a42e986e30d0d805edb7fd75e1aa9bcfa8
Osigelialex <osigelialex@gmail.com>
2024-07-17 11:21:50 +0100

    feat: Add ticket purchase functionality to webhook endpoint

--- src/controllers/paystack.controller.ts
+++ src/controllers/paystack.controller.ts
@@ -1,17 +1,20 @@
 import { PaystackService } from "../services/paystack.service";
 import { PaystackServiceImpl } from "../services/impl/paystack.service.impl";
 import { Request, Response } from "express";
-import { Transaction } from "../models/transaction.model";
+import { BuyTicketDto } from "../dto/ticket.dto";
 import { asyncHandler } from "../middleware/asyncHandler.middleware";
 import crypto from "crypto";
+import { TicketService } from "../services/ticket.service";
+import TicketServiceImpl from "../services/impl/ticket.service.impl";
 
 const paystackService: PaystackService = new PaystackServiceImpl();
+const ticketService: TicketService = new TicketServiceImpl();
 
 export const createPaymentSession = asyncHandler(async (req: Request, res: Response) => {
-  const { amount, email } = req.body;
+  const { amount, email, eventId, quantity } = req.body;
 
   try {
-    const { authorizationUrl, reference } = await paystackService.createPaymentSession(amount, email);
+    const { authorizationUrl, reference } = await paystackService.createPaymentSession(amount, email, eventId, quantity);
     res.json({ authorizationUrl, reference });
   } catch (error: any) {
     res.status(500).json({ message: error.message });
@@ -25,23 +28,32 @@ export const webhook = asyncHandler(async (req: Request, res: Response) => {
   const hash = crypto.createHmac('sha512', secretKey).update(JSON.stringify(req.body), 'utf8').digest('hex');
 
   if (hash === req.headers['x-paystack-signature']) {
-    const { event } = req.body;
+    const { event, data } = req.body;
 
     if (event === "charge.success") {
 
-      // save transaction to database
-      const transaction = new Transaction({
-        amount: req.body.data.amount,
-        status: "success",
-        reference: req.body.data.reference,
-        timestamp: new Date(),
+      // send ticket to users email
+      const buyTicketDto = new BuyTicketDto({
+        email: data.customer.email,
+        eventId: data.metadata.eventId,
+        quantity: data.metadata.quantity
       });
 
-      await transaction.save();
+      await ticketService.buyTicket(buyTicketDto);
+
       return res.status(200).json({ message: "Payment successful" });
     } else if (event === "charge.failed") {
       return res.status(400).json({ message: "Payment failed" });
     } else if (event === "transfer.success") {
+      
+      // send ticket to users email
+      const buyTicketDto = new BuyTicketDto({
+        email: event.customer.email,
+        eventId: event.metadata.event_id,
+        quantity: event.metadata.quantity
+      });
+
+      await ticketService.buyTicket(buyTicketDto);
       return res.status(200).json({ message: "Transfer successful" });
     } else if (event === "transfer.failed") {
       return res.status(400).json({ message: "Transfer failed" });
@@ -53,10 +65,4 @@ export const webhook = asyncHandler(async (req: Request, res: Response) => {
   } else {
     return res.status(400).json({ message: "Invalid signature" });
   }
-});
-
-export const verifyTransaction = asyncHandler(async (req: Request, res: Response) => {
-  const { reference } = req.body;
-  const status = await paystackService.verifyTransaction(reference);
-  return res.status(200).json({ status });
 });
--- src/routes/paystack.routes.ts
+++ src/routes/paystack.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { createPaymentSession, webhook, verifyTransaction } from "../controllers/paystack.controller";
+import { createPaymentSession, webhook } from "../controllers/paystack.controller";
 
 const router = Router();
 
 router.post("/create-payment-session", createPaymentSession);
 router.post("/webhook", webhook);
-router.post("/verify-transaction", verifyTransaction);
 
 export default router;
